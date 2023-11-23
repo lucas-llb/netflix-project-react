dab8008c6ba999dedee85ef1758e11a79adfc41d
Lucas Luciano Barbosa <lucas.lbarbosa@ambevtech.com.br>
2023-11-23 14:00:24 -0300

    initial commit

---
+++ src/app/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '../styles/homeNoAuth.module.scss'
+
+const HomeNoAuth = () => {
+    return (
+        <>
+        <Head>
+            Home
+        </Head>
+        <main></main>
+        </>
+    )
+};
+
+export default HomeNoAuth;
