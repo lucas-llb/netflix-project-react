276b2b8d2e6ff2da70397793141062195137bd0f
Lucas Luciano Barbosa <lucas.lbarbosa@ambevtech.com.br>
2023-12-12 16:29:47 -0300

    finished slide section

--- src/components/common/slideComponent/index.tsx
+++ src/components/common/slideComponent/index.tsx
@@ -8,14 +8,41 @@ interface props {
 }
 
 const SlideComponent = function ({serie}: props) {
+    let slideCount = 0;
+
+    if(serie.length > 4){
+        slideCount = 4
+    } else if (serie) {
+        slideCount = serie.length;
+    }
     return (
         <>
-        <div>
+        <div className="d-flex flex-column align-items-center py-5">
             <Splide options={{
                 type: "loop",
-                perPage: 4,
+                perPage: slideCount,
                 perMove: 1,
+                width: slideCount*300,
                 pagination: false,
+                arrows: serie.length > 4 ? true : false,
+                drag: serie.length > 4 ? true : false,
+                breakpoints: {
+                    1200: {
+                        perPage: slideCount >= 2 ? 2 : 1,
+                        width: slideCount >= 2 ? 600 : 300,
+                        arrows: serie.length > 2 ? true : false,
+                        drag: serie.length > 2 ? true : false,
+                    },
+                    600: {
+                        perPage: 1,
+                        width: 300,
+                        arrows: serie.length > 1 ? true : false,
+                        drag: serie.length > 1 ? true : false,
+                    },
+                    300: {
+                        width: 250,
+                    }
+                }
             }}>
                 {serie?.map((serie) => (
                     <SplideSlide key={serie.id}>
